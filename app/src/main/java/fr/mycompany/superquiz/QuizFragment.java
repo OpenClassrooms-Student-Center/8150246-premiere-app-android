2db0463cf7c5d2814f871b0cd0e76a4298a2aa08
DEMEY Fanny <fanny.pluvinage@gmail.com>
2023-03-10 15:38:33 +0100

    partie 2

---
+++ app/src/main/java/fr/mycompany/superquiz/QuizFragment.java
@@ -0,0 +1,34 @@
+package fr.mycompany.superquiz;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link QuizFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class QuizFragment extends Fragment {
+
+    public static QuizFragment newInstance() {
+        QuizFragment fragment = new QuizFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+}
