847e7cf1e04970acb9c4c787910ba4a2c4dde209
DEMEY Fanny <fanny.pluvinage@gmail.com>
2023-03-10 15:38:33 +0100

    partie 2

--- app/src/main/java/fr/mycompany/superquiz/WelcomeFragment.java
+++ app/src/main/java/fr/mycompany/superquiz/WelcomeFragment.java
@@ -1,27 +1,63 @@
 package fr.mycompany.superquiz;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import fr.mycompany.superquiz.databinding.FragmentWelcomeBinding;
+
 
 public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding binding;
     public static WelcomeFragment newInstance() {
-        WelcomeFragment fragment = new WelcomeFragment();
-        return fragment;
+        return new WelcomeFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.playButton.setEnabled(false);
+        binding.usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.playButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+        binding.playButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                QuizFragment quizFragment = QuizFragment.newInstance();
+                fragmentTransaction.add(R.id.container, quizFragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
